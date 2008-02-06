2196469f0e23330b7fb4a02a5c3f01bf25d096af
Rich Bellamy <rich@rmbwebs.com>
2008-02-06 18:43:34 +0000

    Added LGPL license

--- class_DatabaseMagicObject.php
+++ class_DatabaseMagicObject.php
@@ -1,4 +1,25 @@
 <?php
+/*******************************************
+	Copyright Rich Bellamy, RMB Webs, 2008
+	Contact: rich@rmbwebs.com
+	
+	This file is part of Database Magic.
+	
+	Database Magic is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	
+	Database Magic is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Lesser General Public License for more details.
+	
+	You should have received a copy of the GNU Lesser General Public License
+	along with Database Magic.  If not, see <http://www.gnu.org/licenses/>.
+*******************************************/
+
+
 
 
 /**
