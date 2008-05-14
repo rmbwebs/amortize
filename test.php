09fcf7408e32fd065ef99c9e0266f620cb5ba111
Rich Bellamy <rich@rich-desktop.(none)>
2008-05-14 00:27:13 -0400

    more changes to test.php

--- test.php
+++ test.php
@@ -9,20 +9,20 @@ include_once 'databasemagic.php';
 	<head>
 		<title>DatabaseMagic Testing Script</title>
 		<style>
-			pre.info    {font-size: 1.1em; color: green;}
-			pre.query   {border: 2px solid; margin-left: 1em;}
-			pre.regular {border-color: blue;}
-			pre.system  {border-color: red; margin-left: 2em;}
+			pre.info    {font-size: 1.2em; color: green;}
+			pre.query   {border: 2px solid;}
+			pre.regular {border-color: blue; margin-left: 1em;}
+			pre.system  {border-color: red;  margin-left: 2em;}
 			pre.error   {font-weight: bold; color: red;}
-			pre.heading {font-size: 1.2em; color: orange;}
+			pre.heading {font-size: 2em; color: orange;}
 		</style>
 	</head>
 	<body>
 <?php
 
-class Publisher extends PrimaryDatabaseMagicObject {
+class Collection extends PrimaryDatabaseMagicObject {
 	protected $table_defs = array(
-		'publishers' => array(
+		'collections' => array(
 			'name'   => "tinytext"
 		)
 	);
@@ -49,14 +49,9 @@ class Review extends PrimaryDatabaseMagicObject {
 	);
 }
 
-dbm_debug("info", "Creating a fresh publisher");
-$pub = new Publisher();
-$pub->setAttribs(array('name' => "RMB Publications"));
-$pub->save();
-$pub->dumpview(true);
+dbm_debug("heading", "Testing object creation, saving and loading. . .");
 
-
-dbm_debug("info", "Creating Monster Hunter Internationl");
+dbm_debug("info", "Creating the Book \"Monster Hunter International\"");
 $aBook = new Book();
 $aBook->setAttribs(
 	array(
@@ -71,9 +66,15 @@ dbm_debug("info", "Saving book . . . ");
 $aBook->save();
 dbm_debug("info", "done.  ");
 $aBook->dumpview(true);
+$id = $aBook->getPrimary();
+dbm_debug("info", "The database gave this book a key value of {$id}.  Loading Book({$id}). . .");
+$newBook = new Book($id);
+dbm_debug("info", "Comparing the two:");
+$aBook->dumpview(true);
+$newBook->dumpview(true);
 
 
-dbm_debug("info", "Creating The Art of the Rifle");
+dbm_debug("info", "Creating the Book \"The Art of the Rifle\"");
 $aBook = new Book();
 $aBook->setAttribs(
 	array(
@@ -83,14 +84,11 @@ $aBook->setAttribs(
 		'pubyear' => "2002"
 	)
 );
-$aBook->dumpview(true);
-dbm_debug("info", "Saving book . . . ");
 $aBook->save();
-dbm_debug("info", "done.  ");
 $aBook->dumpview(true);
 
 
-dbm_debug("info", "Creating The Revolution: A Manifesto");
+dbm_debug("info", "Creating the Book \"The Revolution: A Manifesto\"");
 $aBook = new Book();
 $aBook->setAttribs(
 	array(
@@ -100,13 +98,17 @@ $aBook->setAttribs(
 		'pubyear' => "2008"
 	)
 );
-$aBook->dumpview(true);
-dbm_debug("info", "Saving book . . . ");
 $aBook->save();
-dbm_debug("info", "done.  ");
 $aBook->dumpview(true);
 
+dbm_debug("info", "Creating the Collection \"Rich's Favorite Books\"");
+$pub = new collection();
+$pub->setAttribs(array('name' => "Rich's Favorite Books"));
+$pub->save();
+$pub->dumpview(true);
 
+
+dbm_debug("heading", "Testing linking functions. . .");
 
 dbm_debug("info", "Loading book 2 . . . ");
 $greatBook = new Book(2);  // Loads Cooper's book from the database
@@ -170,50 +172,50 @@ foreach ($books as $book) {
 
 dbm_debug("heading", "Beginning testing for relational linking.");
 
-$publisher = new Publisher(1);
+$collection = new Collection(1);
 $mhi  = new Book(1);
 $aotr = new Book(2);
 $tram = new Book(3);
 
-dbm_debug("info", "Loaded a publisher and three books:");
-$publisher->dumpview(true);
+dbm_debug("info", "Loaded a collection and three books:");
+$collection->dumpview(true);
 $mhi->dumpview(true);
 $aotr->dumpview(true);
 $tram->dumpview(true);
 
-dbm_debug("info", "Creating a relational link \"Sci-Fi\" from publisher 1 to book 1");
-$publisher->link($mhi, "Sci-Fi");
+dbm_debug("info", "Creating a relational link \"Sci-Fi\" from collection 1 to book 1");
+$collection->link($mhi, "Sci-Fi");
 
-dbm_debug("info", "Creating a relational link \"guns\" from publisher 1 to book 1");
-$publisher->link($mhi, "guns");
+dbm_debug("info", "Creating a relational link \"guns\" from collection 1 to book 1");
+$collection->link($mhi, "guns");
 
-dbm_debug("info", "Creating a relational link \"guns\" from publisher 1 to book 2");
-$publisher->link($aotr, "guns");
+dbm_debug("info", "Creating a relational link \"guns\" from collection 1 to book 2");
+$collection->link($aotr, "guns");
 
-dbm_debug("info", "Creating a blank link from publisher 1 to book 3");
-$publisher->link($tram);
+dbm_debug("info", "Creating a blank link from collection 1 to book 3");
+$collection->link($tram);
 
 
 dbm_debug("info", "Attempting to find all books labeled \"Sci-Fi\" (should just be Monster Hunter International)");
-$books = $publisher->getLinks("Book", "Sci-Fi");
+$books = $collection->getLinks("Book", "Sci-Fi");
 foreach ($books as $book) {
 	$book->dumpview(true);
 }
 
 dbm_debug("info", "Attempting to find all books labeled \"guns\" (should just be Monster Hunter International and Art of the Rifle)");
-$books = $publisher->getLinks("Book", "guns");
+$books = $collection->getLinks("Book", "guns");
 foreach ($books as $book) {
 	$book->dumpview(true);
 }
 
 dbm_debug("info", "Attempting to find all blank-linked books (should just be The Revolution)");
-$books = $publisher->getLinks("Book");
+$books = $collection->getLinks("Book");
 foreach ($books as $book) {
 	$book->dumpview(true);
 }
 
 dbm_debug("info", "Attempting to find all linked books (should be all three books with no repeats)");
-$books = $publisher->getLinks("Book", true);
+$books = $collection->getLinks("Book", true);
 foreach ($books as $book) {
 	$book->dumpview(true);
 }
