46665677130872c80e0e677e6bf12b6ae577bfed
Rich Bellamy <rich@rmbmobile.(none)>
2008-05-20 11:54:13 -0400

    Fixed some errors in the Extras classes

--- classes_Extras.php
+++ classes_Extras.php
@@ -21,96 +21,6 @@
 
 
 	include_once 'databasemagic.php';
-	/**
- * The User class is a generic class that allows for logging in and out of
- * a custom app.
- */
-class DbMExtras_User extends DatabaseMagicObjectForms {
-	protected $table_defs = array(
-		'Users' => array(
-			'login'    => array("varchar(10)", "NO",  "PRI"),
-			'password' => "tinytext",
-			'type'     => "varchar(10)"
-		)
-	);
-
-	protected $omegaMan = false;
-
-	/// Most DMB Objects do not get their IDs set by the constructor, but by the load function.
-	/// Users have to be an exception in the event that you build a user-driven website and can't login
-	function __construct($id = NULL) {
-		// Call the default constructor
-		parent::__construct($id);
-		// Check for proper loading
-		if ($this->getPrimary() != $id) {
-			// The load didn't occur properly, check if there are any users in the DB
-			if (!$this->getAllPrimaries(1)) {
-				// I am the Omega Man!
-				$this->omegaMan = true;
-				$this->setAttribs(array('login' => $id, 'type' => "superadmin"));
-			}
-		}
-	}
-
-	function save($force=false) {
-		$a = $this->getAttribs();
-		if ($a['type'] != "admin" && $a['type'] != "superadmin") {
-			$this->setAttribs(array('type' => "regular"));
-		}
-		parent::save($force);
-	}
-
-	function checkPass($pass) {
-		$attribs = $this->getAttribs();
-
-		if (strlen($this->getPrimary()) != 0) {
-			// Not a blank user
-			if ($pass == $attribs['password']) {
-				// password matches what we have on record
-				return true;
-			} else {
-				// Password didn't match, but lets check if that's because there are no users
-				if ($this->omegaMan) {
-					// Apparently, we discovered that there were no users in the DB, so now that we have a password, let's save this user
-					$this->setattribs(array('password' => $pass));
-					$this->save();
-					// Indicate that this was the proper password.
-					return true;
-				}
-			}
-		} else {
-			// Blank user primary
-			return false;
-		}
-	}
-
-	function welcomeMessage($which=null) {
-		$pri = $this->getPrimary();
-		$attribs=$this->getattribs();
-		$sal = ($which != null) ? $attribs[$which] : $pri;
-		$class = get_class($this);
-		echo
-<<<welcome
-	<span id="{$class}_{$pri}_welcome" class="{$class}_welcome">Welcome, {$sal}.</span>\n
-welcome;
-	}
-
-	function logoutForm($action=null) {
-		$action = (!is_null($action)) ? " action=\"{$action}\"" : "";
-		$class = get_class($this);
-		echo
-<<<logout
-	<form{$action} method="POST" class="${class}_logout">
-		<input type="submit" name="loginaction" value="logout" />
-	</form>\n
-logout;
-	}
-
-	function loginForm($action=null) {
-		$this->inputForm(array("login", "password"), $action, $hidden=array('loginaction'=>"login"));
-	}
-
-}
 
 /**
  * This is an extension of DatabaseMagicObject that provides form processing through a DOMDocument
@@ -138,7 +48,7 @@ class DatabaseMagicObjectDomForms extends DatabaseMagicObject {
 
 		foreach ($which as $field) {
 			if (!isset($hidden[$field])) {
-				$returnMe->appendChild($this->domDisplayField($dom, $field));
+				$returnMe->appendChild($this->displayField($dom, $field));
 			}
 		}
 
@@ -446,6 +356,98 @@ end;
  */
 class PrimaryDatabaseMagicObjectForms extends DatabaseMagicObjectForms {
 	protected $table_defs = array("databasemagic" => array('ID'=> array("bigint(20) unsigned", "NO",  "PRI", "", "auto_increment") ) );
+}
+
+
+	/**
+ * The User class is a generic class that allows for logging in and out of
+ * a custom app.
+ */
+class DbMExtras_User extends DatabaseMagicObjectForms {
+	protected $table_defs = array(
+		'Users' => array(
+			'login'    => array("varchar(10)", "NO",  "PRI"),
+			'password' => "tinytext",
+			'type'     => "varchar(10)"
+		)
+	);
+
+	protected $omegaMan = false;
+
+	/// Most DMB Objects do not get their IDs set by the constructor, but by the load function.
+	/// Users have to be an exception in the event that you build a user-driven website and can't login
+	function __construct($id = NULL) {
+		// Call the default constructor
+		parent::__construct($id);
+		// Check for proper loading
+		if ($this->getPrimary() != $id) {
+			// The load didn't occur properly, check if there are any users in the DB
+			if (!$this->getAllPrimaries(1)) {
+				// I am the Omega Man!
+				$this->omegaMan = true;
+				$this->setAttribs(array('login' => $id, 'type' => "superadmin"));
+			}
+		}
+	}
+
+	function save($force=false) {
+		$a = $this->getAttribs();
+		if ($a['type'] != "admin" && $a['type'] != "superadmin") {
+			$this->setAttribs(array('type' => "regular"));
+		}
+		parent::save($force);
+	}
+
+	function checkPass($pass) {
+		$attribs = $this->getAttribs();
+
+		if (strlen($this->getPrimary()) != 0) {
+			// Not a blank user
+			if ($pass == $attribs['password']) {
+				// password matches what we have on record
+				return true;
+			} else {
+				// Password didn't match, but lets check if that's because there are no users
+				if ($this->omegaMan) {
+					// Apparently, we discovered that there were no users in the DB, so now that we have a password, let's save this user
+					$this->setattribs(array('password' => $pass));
+					$this->save();
+					// Indicate that this was the proper password.
+					return true;
+				}
+			}
+		} else {
+			// Blank user primary
+			return false;
+		}
+	}
+
+	function welcomeMessage($which=null) {
+		$pri = $this->getPrimary();
+		$attribs=$this->getattribs();
+		$sal = ($which != null) ? $attribs[$which] : $pri;
+		$class = get_class($this);
+		echo
+<<<welcome
+	<span id="{$class}_{$pri}_welcome" class="{$class}_welcome">Welcome, {$sal}.</span>\n
+welcome;
+	}
+
+	function logoutForm($action=null) {
+		$action = (!is_null($action)) ? " action=\"{$action}\"" : "";
+		$class = get_class($this);
+		echo
+<<<logout
+	<form{$action} method="POST" class="${class}_logout">
+		<input type="submit" name="loginaction" value="logout" />
+	</form>\n
+logout;
+	}
+
+	function loginForm($action=null) {
+		$this->inputForm(array("login", "password"), $action, $hidden=array('loginaction'=>"login"));
+	}
+
 }
 
 
