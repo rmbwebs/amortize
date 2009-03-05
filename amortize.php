bf215a77f07e5c4323e8b713c42fc9fad2fb5c32
Rich Bellamy <rich@rmbmobilecommand.(none)>
2009-03-04 23:08:35 -0500

    Added the copyright notice to amortize.php

--- amortize.php
+++ amortize.php
@@ -1,4 +1,24 @@
 <?php
+/*******************************************
+	Copyright Rich Bellamy, RMB Webs, 2008
+	Contact: rich@rmbwebs.com
+
+	This file is part of Amortize.
+
+	Amortize is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Amortize is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public License
+	along with Amortize.  If not, see <http://www.gnu.org/licenses/>.
+*******************************************/
+
 	include_once dirname(__FILE__) . '/class_AmortizeInterface.php';
 	class Amortize extends AmortizeInterface {}
 ?>
