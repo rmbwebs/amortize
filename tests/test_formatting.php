6af9e5ea5a93e0108c768e9f7c42e280791a32e8
Rich Bellamy <rich@rmbwebs.com>
2009-04-13 13:50:29 -0400

    Upgraded the autoconversion test

--- tests/test_formatting.php
+++ tests/test_formatting.php
@@ -10,21 +10,27 @@ class FormattingTest extends Amortize {
 	protected $autoprimary = true;
 }
 
-dbm_debug('heading', 'Dropping testing tables');
-$objects = array(
-	new FormattingTest,
-);
-foreach ($objects as $class) {
-	$class->dropTable();
-}
-unset($class);
-dbm_debug('heading', 'Done dropping testing tables');
-
-
 $test = new FormattingTest();
-$test->someDate = "Last Tuesday";
+
+dbm_debug('info', 'Setting the info:');
+
+$test->someDate    = "Last Tuesday";
 $test->pointInTime = "Saturday, 5:00 PM";
-$test->aTime = "noon";
+$test->aTime       = "Noon";
+
+dbm_debug('info', 'Doing the save: The SQL statement should have sql-compatible time and date strings.');
+
 $test->save();
 
+dbm_debug('heading', "End of test.");
+
+// Code below deals with dropping the the testing table.
+if (isset($_POST['drop']) && $_POST['drop'] == "Drop Testing Table") {
+	$test->dropTable();
+}
+
 ?>
+<form method="POST">
+<div class="info">You can drop the test table if you want to see the table recreated on the next run, or if you just want to clean the testing DB</div>
+<input name="drop" type="submit" value="Drop Testing Table" />
+</form>
