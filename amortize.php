01a248f68e34ade9a806d85e55c52a785818a290
Rich Bellamy <rich@rmbwebs.com>
2009-08-25 02:10:01 -0400

    Moved the utils code to amortize.php

--- amortize.php
+++ amortize.php
@@ -61,7 +61,71 @@
 	if (!defined('DBM_DROP_TABLES'))     { define('DBM_DROP_TABLES',     $dbmTableDrop); }
 		else if (!$dbmAllowConfOverrides)  { die('DBM_DROP_TABLES was previously defined as "'.DBM_DROP_TABLES.'" and that is not allowed per '.$configFile); }
 
-require_once dirname(__FILE__) . '/amortizeutils.php';
+function first_val($arr = array()) {
+	if (is_array($arr) && count($arr) > 0) {
+		$vals = array_values($arr);
+		return $vals[0];
+	} else {
+		return null;
+	}
+}
+
+function first_key($arr = array()) {
+	if (is_array($arr) && count($arr) > 0) {
+		$keys = array_keys($arr);
+		return $keys[0];
+	} else {
+		return null;
+	}
+}
+
+function dbm_debug($class, $message) {
+	if (DBM_DEBUG) {
+		if (is_string($message)) {
+			echo "<div class=\"$class\">";
+				echo $message;
+			echo "\n</div>\n";
+		} else {
+			echo "<pre class=\"$class\">\n";
+// 				var_dump($message);
+// 				print_r($message);
+				var_export($message);
+			echo "\n</pre>\n";
+		}
+	}
+}
+
+
+if (DBM_DEBUG) { set_error_handler ("dbm_do_backtrace"); }
+
+function dbm_do_backtrace ($one, $two) {
+	echo "<pre>\nError {$one}, {$two}\n";
+	debug_print_backtrace();
+	echo "</pre>\n\n";
+}
+
+$_SERVER['amtz_query_time'] = 0;
+$_SERVER['amtz_queries']    = array();
+
+function amtz_query($query, $connection=null) {
+	$startTime   = microtime(true);
+	$result      = mysql_query($query, $connection);
+	$endTime     = microtime(true);
+	$elapsedTime = $endTime - $startTime;
+	$_SERVER['amtz_queries'][] = array(
+		'startTime'   => $startTime,
+		'endTime'     => $endTime,
+		'elapsedTime' => $elapsedTime,
+		'query'       => $query
+	);
+	$_SERVER['amtz_query_time'] += $elapsedTime;
+	return $result;
+}
+
+define('E_SQL_CANNOT_CONNECT', "
+<h2>Cannot connect to SQL Server</h2>
+There is an error in your Amortize configuration.
+");
 
 
 /// A class for doing SQL operations automatically on a particular table
